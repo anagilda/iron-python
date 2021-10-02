09ccd8e967dd226a1a5011a156012acaded6b61f
Ana <agdr20@gmail.com>
2021-10-02 21:21:30 +0100

    Add encryption key class

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,21 @@
 """
 Provide constants for encryption and decryption with iron.
 """
+from dataclasses import dataclass
+
 from Crypto.Cipher import AES
+
+
+@dataclass
+class EncryptionKey:
+    """
+    Encryption key implementation.
+    """
+
+    key: bytes = b''
+    salt: str = ''
+    initialization_vector: bytes = b''
+
 
 ENCRYPTION_DEFAULT_OPTIONS = {
     'salt_bits': 256,
